ce1452267c700ef68e27a8524703a8dfc7b6264a
Donkó Márk <donkomark@gmail.com>
2020-04-21 13:27:56 +0200

    Spring boot login alapok létrehozva

---
+++ progtech/src/main/java/hu/vlngfw/progtech/ProgtechApplication.java
@@ -0,0 +1,13 @@
+package hu.vlngfw.progtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgtechApplication.class, args);
+	}
+
+}
---
+++ progtech/src/test/java/hu/vlngfw/progtech/ProgtechApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.vlngfw.progtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgtechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
