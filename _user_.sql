5868001ae2e2a2df1ccc5fac6cba9dfe317c1bae
Donkó Márk <donkomark@gmail.com>
2020-05-12 21:00:05 +0200

    User adatbázis

---
+++ _user_.sql
@@ -0,0 +1,11 @@
+insert into "user" (id, fname, lname, admin, email, password, username) values (1, 'Nollie', 'Cowtherd', false, 'ncowtherd0@sphinn.com', 'XmzTR2', 'ncowtherd0');
+insert into "user" (id, fname, lname, admin, email, password, username) values (2, 'Frants', 'Ashdown', true, 'fashdown1@goo.ne.jp', 'YrjBDJ', 'fashdown1');
+insert into "user" (id, fname, lname, admin, email, password, username) values (3, 'Matelda', 'Eltone', true, 'meltone2@posterous.com', 'qysxhy', 'meltone2');
+insert into "user" (id, fname, lname, admin, email, password, username) values (4, 'Alix', 'Wiltshier', true, 'awiltshier3@wired.com', 'U6KBuYp', 'awiltshier3');
+insert into "user" (id, fname, lname, admin, email, password, username) values (5, 'Rebbecca', 'Bielfeldt', false, 'rbielfeldt4@latimes.com', 'KlVXrPZjmV', 'rbielfeldt4');
+insert into "user" (id, fname, lname, admin, email, password, username) values (6, 'Catarina', 'Iorns', false, 'ciorns5@msu.edu', 'SWWSLR1LJh', 'ciorns5');
+insert into "user" (id, fname, lname, admin, email, password, username) values (7, 'Wilt', 'Sturman', false, 'wsturman6@constantcontact.com', 'k7dolMkk', 'wsturman6');
+insert into "user" (id, fname, lname, admin, email, password, username) values (8, 'Inna', 'Janiak', true, 'ijaniak7@w3.org', 'DLVukE', 'ijaniak7');
+insert into "user" (id, fname, lname, admin, email, password, username) values (9, 'Tremaine', 'Finker', true, 'tfinker8@amazon.co.jp', 'uGWWA1YqI', 'tfinker8');
+insert into "user" (id, fname, lname, admin, email, password, username) values (10, 'Lizzy', 'Karpychev', true, 'lkarpychev9@soundcloud.com', '5mKwnpTZjd', 'lkarpychev9');
+insert into "user" (id, fname, lname, admin, email, password, username) values (11, 'Admin', 'Admin', true, 'Admin@Admin.com', 'Admin', 'Admin');
